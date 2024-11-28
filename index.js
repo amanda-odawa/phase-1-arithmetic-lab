b5aaee562d7f4f9b377e57636b8016582173230d
AmandaOdawa <amandaodawa1@gmail.com>
2024-11-28 18:45:40 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+//Test1
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+
+//Test2
+let random = Math.floor(Math.random()*10000)+1
+
+//Test3
+let num3 = 14
+let num4 = 5
+let mod = num3 % num4
+
+//Test4
+let max = Math.max(5, 10, 15, 20)
